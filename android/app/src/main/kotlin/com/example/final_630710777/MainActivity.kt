bd42d803b7983a91549607a6b4f47cd51eecd547
Anan Wattanaviroon <jameza0456_126852@hotmail.com>
2023-10-26 10:42:43 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/final_630710777/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.final_630710777
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screen/home/home_pages.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Column(children: [
+          Padding(
+            padding: EdgeInsets.all(0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Bangkok',
+                    style:
+                        TextStyle(
+                            fontSize: 60.0,
+                            fontWeight: FontWeight.bold
+                        )
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(2),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Thailand',
+                    style:
+                        TextStyle(fontSize: 18, fontWeight: FontWeight.bold
+                        )
+                ),
+                Text('2023-10-26 09:30',
+                  style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black12
+                  ),
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset('assets/images/10.png', width: 200
+                ),
+                Text('Light rain shower',
+                    style:
+                    TextStyle(fontSize: 18, fontWeight: FontWeight.bold
+                    )
+                ),
+              ],
+            ),
+          ),
+          Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('27.8',
+                        style:
+                        TextStyle(
+                            fontSize: 60.0,
+                            fontWeight: FontWeight.bold
+                        )
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Feels like 32',
+                      style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.red
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('°C',
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(2),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('°F',
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black12
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+          )
+        ]
+        ),
+      ),
+    );
+  }
+}
